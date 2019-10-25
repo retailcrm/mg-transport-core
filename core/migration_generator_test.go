cd50039247b8e928c4f9b0e47a88edad893b7e52
Pavel <pashok9825@gmail.com>
2019-10-25 13:30:24 +0300

    run previously useless test

--- core/migration_generator_test.go
+++ core/migration_generator_test.go
@@ -1,11 +1,15 @@
 package core
 
 import (
+	"fmt"
 	"io/ioutil"
 	"log"
+	"math/rand"
 	"os"
 	"path"
 	"strings"
+	"testing"
+	"time"
 
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/suite"
@@ -18,6 +22,15 @@ type MigrationGeneratorSuite struct {
 
 func (s *MigrationGeneratorSuite) SetupSuite() {
 	s.command = &NewMigrationCommand{Directory: "/tmp"}
+}
+
+func (s *MigrationGeneratorSuite) Test_FileExists() {
+	var (
+		seededRand *rand.Rand = rand.New(rand.NewSource(time.Now().UnixNano()))
+		notExist              = fmt.Sprintf("/tmp/%d", seededRand.Int31())
+	)
+
+	assert.False(s.T(), s.command.FileExists(notExist))
 }
 
 func (s *MigrationGeneratorSuite) Test_Execute() {
@@ -36,4 +49,8 @@ func (s *MigrationGeneratorSuite) Test_Execute() {
 	}
 
 	assert.True(s.T(), found)
+}
+
+func Test_MigrationGenerator(t *testing.T) {
+	suite.Run(t, new(MigrationGeneratorSuite))
 }
