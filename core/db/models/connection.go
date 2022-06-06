b0c2ca5ab122203c287111255543266f39cf97f4
Tyschitskaya Maria <tyschitskaya@retailcrm.ru>
2022-06-06 09:30:17 +0300

    camelCase json parameters in GateUrl & GateToken fields

--- core/db/models/connection.go
+++ core/db/models/connection.go
@@ -8,8 +8,8 @@ type Connection struct {
 	UpdatedAt time.Time
 	Key       string    `gorm:"column:api_key; type:varchar(100); not null" json:"apiKey,omitempty" binding:"required,max=100"`                // nolint:lll
 	URL       string    `gorm:"column:api_url; type:varchar(255); not null" json:"apiUrl,omitempty" binding:"required,validateCrmURL,max=255"` // nolint:lll
-	GateURL   string    `gorm:"column:mg_url; type:varchar(255); not null;" json:"mg_url,omitempty" binding:"max=255"`
-	GateToken string    `gorm:"column:mg_token; type:varchar(100); not null; unique" json:"mg_token,omitempty" binding:"max=100"` // nolint:lll
+	GateURL   string    `gorm:"column:mg_url; type:varchar(255); not null;" json:"mgUrl,omitempty" binding:"max=255"`
+	GateToken string    `gorm:"column:mg_token; type:varchar(100); not null; unique" json:"mgToken,omitempty" binding:"max=100"` // nolint:lll
 	ClientID  string    `gorm:"column:client_id; type:varchar(70); not null; unique" json:"clientId,omitempty"`
 	Accounts  []Account `gorm:"foreignkey:ConnectionID" json:"accounts"`
 	ID        int       `gorm:"primary_key" json:"id"`
