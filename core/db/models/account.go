6dd1b801a02017552d826df3f2949477d86b5505
Maria Tyschitskaya <tyschitskaya@retailcrm.ru>
2022-08-10 14:32:35 +0300

    lang field in connection

--- core/db/models/account.go
+++ core/db/models/account.go
@@ -8,7 +8,6 @@ type Account struct {
 	UpdatedAt           time.Time
 	ChannelSettingsHash string `gorm:"column:channel_settings_hash; type:varchar(70)" binding:"max=70"`
 	Name                string `gorm:"column:name; type:varchar(100)" json:"name,omitempty" binding:"max=100"`
-	Lang                string `gorm:"column:lang; type:varchar(2)" json:"lang,omitempty" binding:"max=2"`
 	Channel             uint64 `gorm:"column:channel; not null; unique" json:"channel,omitempty"`
 	ID                  int    `gorm:"primary_key" json:"id"`
 	ConnectionID        int    `gorm:"column:connection_id" json:"connectionId,omitempty"`
